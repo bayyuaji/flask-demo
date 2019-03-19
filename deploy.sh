34d581bdc4a415f3b90e6da3a4f99f988c1ee9b0
Debian <admin@ip-10-0-101-60.ap-southeast-1.compute.internal>
2019-03-19 21:12:39 +0700

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+eval "$(ssh-agent -s)"
+ssh-add -k ~/.ssh/id_rsa
+git pull
+
+echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
+docker stop helloworld
+docker rm helloworld
+docker run -d --name helloworld -p 5000:5000 ajisepulsa/helloworld:latest
+
