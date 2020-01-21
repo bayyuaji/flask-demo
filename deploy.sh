7ce05529cd09251f905a0993b4fbfc20e9a2c967
Debian <admin@ip-10-0-101-60.ap-southeast-1.compute.internal>
2020-01-21 10:35:32 +0700

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@
 
 eval "$(ssh-agent -s)" &&
 ssh-add -k ~/.ssh/id_rsa &&
-cd /var/www/helloworld
+cd /var/www/backend #helloworld
 git pull
 
 source ~/.profile
 echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
-docker stop helloworld
-docker rm helloworld
-docker rmi ajisepulsa/helloworld
-docker run -d --name helloworld -p 5000:5000 ajisepulsa/helloworld:latest
+docker stop backend
+docker rm backend
+docker rmi ajisepulsa/alta
+docker run -d --name backend -p 5000:5000 ajisepulsa/alta:latest
 
