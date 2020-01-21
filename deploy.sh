6c5377b373828b22da105fb54ed32e87baec079b
Debian <admin@ip-10-0-101-60.ap-southeast-1.compute.internal>
2020-01-21 13:26:49 +0700

    demo 2

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ source ~/.profile
 echo "$DOCKERHUB_PASS" | docker login --username $DOCKERHUB_USER --password-stdin
 docker stop backend
 docker rm backend
-docker rmi ajisepulsa/alta
+docker rmi ajisepulsa/alta:latest
 docker run -d --name backend -p 5000:5000 ajisepulsa/alta:latest
 
